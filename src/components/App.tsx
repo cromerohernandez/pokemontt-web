1b78d921f14c0cf5e240583b64b2520d92844590
CarlosRomeroHernandez <c.romero.hsv@gmail.com>
2022-08-13 12:36:12 +0200

    start project

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../stylesheets/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Pokemontt Arena</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
