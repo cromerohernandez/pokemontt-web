8c7cb83a62929f25fc7bf5e9096af1167fffb521
CarlosRomeroHernandez <c.romero.hsv@gmail.com>
2022-09-14 23:46:33 +0200

    deploy testing

--- src/services/PokemonttService.tsx
+++ src/services/PokemonttService.tsx
@@ -4,8 +4,7 @@ import { IAttackData } from '../utils/models/battle.models';
 import { ISettingsDataForRequest, IUserDataForRequest } from '../utils/models/user.models';
 
 const http = axios.create({
-  baseURL: /*process.env.REACT_APP_API_URL ||*/ 'http://localhost:5000',
-  //headers: {'Access-Control-Allow-Origin': '*'},
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
   withCredentials: true,
 });
 
